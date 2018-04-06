1d632c6b9e63651dc271cb8c41ba1e66ac795d4d
Arlin Grijalba <arlin.grijalba@greatcall.com>
2018-04-06 09:10:28 -0700

    Challenge #6

--- lighthouse.js
+++ lighthouse.js
@@ -1,15 +1,18 @@
+// Challenge 1
 function gridSize(){
   height = GRID.length;
   width = GRID[0].length;
   return (width.toString() + " x " + height.toString());
 }
 
+// Challenge 2
 function totalCells(){
   height = GRID.length;
   width = GRID[0].length;
   return height * width;
 }
 
+// Challenge 3
 function lightCell(coordinate){
   letter = getLetter(coordinate);
   number = getNumber(coordinate);
@@ -19,6 +22,7 @@ function lightCell(coordinate){
   return (GRID[y])[x];
 }
 
+// Challenge 4
 function isRock(coordinate){
   letter = getLetter(coordinate);
   number = getNumber(coordinate);
@@ -28,6 +32,7 @@ function isRock(coordinate){
   return (GRID[y][x] == '^')
 }
 
+// Challenge 5
 function isCurrent(coordinate){
   letter = getLetter(coordinate);
   number = getNumber(coordinate);
@@ -37,6 +42,13 @@ function isCurrent(coordinate){
   return (GRID[y][x] == '~')
 }
 
+// Challenge 6
+function lightRow(row){
+  y = getYIndex(row);
+  return GRID[y];
+}
+
+// Helper functions (Challenge 3)
 function getLetter(coordinate){
   return coordinate[0].toLowerCase();
 }
@@ -49,6 +61,6 @@ function getXIndex(letter){
   return letter.charCodeAt(0) - 97;
 }
 
-function getYIndex(){
+function getYIndex(number){
   return number-1;
 }
