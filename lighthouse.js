48e4e71a8723e9dbacced57b98eb5b65038da80c
Arlin Grijalba <arlin.grijalba@greatcall.com>
2018-04-18 09:03:57 -0700

    Challenge 14

--- lighthouse.js
+++ lighthouse.js
@@ -28,7 +28,7 @@ function totalCells(){
 }
 
 // Challenge 3
-function lightCell(coordinate){
+function lightCellFromCoordinate(coordinate){
   letter = getLetterFromCoordinate(coordinate);
   number = getNumberFromCoordinate(coordinate);
   
@@ -58,7 +58,7 @@ function isValidNumber(number){
 }
 
 // Challenge 9
-function isSafe(coordinate){
+function isSafeFromCoordinate(coordinate){
   letter = getLetterFromCoordinate(coordinate);
   number = getNumberFromCoordinate(coordinate);
   x = getXIndex(letter);
@@ -71,7 +71,7 @@ function isSafe(x,y){
 }
 
 // Challenge 4
-function isRock(coordinate){
+function isRockFromCoordinate(coordinate){
   letter = getLetterFromCoordinate(coordinate);
   number = getNumberFromCoordinate(coordinate);
   x = getXIndex(letter);
@@ -84,7 +84,7 @@ function isRock(x,y){
 }
 
 // Challenge 5
-function isCurrent(coordinate){
+function isCurrentFromCoordinate(coordinate){
   letter = getLetterFromCoordinate(coordinate);
   number = getNumberFromCoordinate(coordinate);
   x = getXIndex(letter);
@@ -160,6 +160,39 @@ function firstCurrent(){
   }
 }
 
+// Challenge 14
+function isDangerousFromCoordinate(coordinate){
+  letter = getLetterFromCoordinate(coordinate);
+  number = getNumberFromCoordinate(coordinate);
+  x = getXIndex(letter);
+  y = getYIndex(number);
+
+  // check cell itself
+  if (!isSafe(x,y))
+    return true;
+  // check cell above
+  if (y > 0){
+    if (!isSafe(x,y-1))
+      return true;
+  }
+  // check cell below
+  if (y < height-1){
+    if (!isSafe(x,y+1))
+      return true;
+  }
+  // check cell to the left
+  if (x > 0){
+    if (!isSafe(x-1,y))
+      return true;
+  }
+  // check cell to the right
+  if (x < width-1){
+    if (!isSafe(x+1,y))
+      return true;
+  }
+  return false;
+}
+
 // Helper functions (Challenge 3)
 function getLetterFromCoordinate(coordinate){
   return coordinate[0].toUpperCase();
@@ -194,12 +227,49 @@ function getColumn(column){
 
 // Examples
 
-console.log(lightCell("Z1"));
-console.log(lightCell("C10"));
-console.log(lightCell("D12"));
-console.log(lightCell("E10"));
-console.log(lightCell("F10"));
-console.log(allRocks());
-console.log(isRock("C8"));
-console.log(isRock("C8"));
-console.log(isRock("B1"));
+// console.log(lightCellFromCoordinate("Z1"));
+// console.log(lightCellFromCoordinate("C10"));
+// console.log(lightCellFromCoordinate("D12"));
+// console.log(lightCellFromCoordinate("E10"));
+// console.log(lightCellFromCoordinate("F10"));
+// console.log(allRocks());
+// console.log(isRockFromCoordinate("C8"));
+// console.log(isRockFromCoordinate("D8"));
+// console.log(isRockFromCoordinate("B1"));
+// console.log(isRockFromCoordinate("F4"));
+
+console.log("Is 'A1' dangerous? " + isDangerousFromCoordinate("A1"));
+console.log("Is 'A2' dangerous? " + isDangerousFromCoordinate("A2"));
+console.log("Is 'A3' dangerous? " + isDangerousFromCoordinate("A3"));
+console.log("Is 'A4' dangerous? " + isDangerousFromCoordinate("A4"));
+console.log("Is 'A5' dangerous? " + isDangerousFromCoordinate("A5"));
+console.log("Is 'A6' dangerous? " + isDangerousFromCoordinate("A6"));
+console.log("Is 'A7' dangerous? " + isDangerousFromCoordinate("A7"));
+console.log("Is 'A8' dangerous? " + isDangerousFromCoordinate("A8"));
+console.log("Is 'A9' dangerous? " + isDangerousFromCoordinate("A9"));
+console.log("Is 'A10' dangerous? " + isDangerousFromCoordinate("A10"));
+
+console.log("Is 'B1' dangerous? " + isDangerousFromCoordinate("B1"));
+console.log("Is 'B2' dangerous? " + isDangerousFromCoordinate("B2"));
+console.log("Is 'B3' dangerous? " + isDangerousFromCoordinate("B3"));
+console.log("Is 'B4' dangerous? " + isDangerousFromCoordinate("B4"));
+console.log("Is 'B5' dangerous? " + isDangerousFromCoordinate("B5"));
+console.log("Is 'B6' dangerous? " + isDangerousFromCoordinate("B6"));
+console.log("Is 'B7' dangerous? " + isDangerousFromCoordinate("B7"));
+console.log("Is 'B8' dangerous? " + isDangerousFromCoordinate("B8"));
+console.log("Is 'B9' dangerous? " + isDangerousFromCoordinate("B9"));
+console.log("Is 'B10' dangerous? " + isDangerousFromCoordinate("B10"));
+
+
+console.log("Is 'D1' dangerous? " + isDangerousFromCoordinate("D1"));
+console.log("Is 'D2' dangerous? " + isDangerousFromCoordinate("D2"));
+console.log("Is 'D3' dangerous? " + isDangerousFromCoordinate("D3"));
+console.log("Is 'D4' dangerous? " + isDangerousFromCoordinate("D4"));
+console.log("Is 'D5' dangerous? " + isDangerousFromCoordinate("D5"));
+console.log("Is 'D6' dangerous? " + isDangerousFromCoordinate("D6"));
+console.log("Is 'D7' dangerous? " + isDangerousFromCoordinate("D7"));
+console.log("Is 'D8' dangerous? " + isDangerousFromCoordinate("D8"));
+console.log("Is 'D9' dangerous? " + isDangerousFromCoordinate("D9"));
+console.log("Is 'D10' dangerous? " + isDangerousFromCoordinate("D10"));
+// console.log(isDangerousFromCoordinate("B9"));
+// console.log(isDangerousFromCoordinate("I6"));
